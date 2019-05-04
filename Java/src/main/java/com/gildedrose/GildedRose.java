54fad8d2c819bb617a70b5fdebb2467cc0c4996e
Brian Button <bbutton@agilestl.com>
2019-05-03 19:15:45 -0500

    Last couple tests

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -9,6 +9,7 @@ class GildedRose {
 
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
+            // if normal item and quality is > 0
             if (!items[i].name.equals("Aged Brie")
                     && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                 if (items[i].quality > 0) {
@@ -16,10 +17,12 @@ class GildedRose {
                         items[i].quality = items[i].quality - 1;
                     }
                 }
+            // if aged brie or backstage passes
             } else {
                 if (items[i].quality < 50) {
                     items[i].quality = items[i].quality + 1;
 
+                    // special backstage passes quality calculations
                     if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                         if (items[i].sellIn < 11) {
                             if (items[i].quality < 50) {
@@ -36,11 +39,14 @@ class GildedRose {
                 }
             }
 
+            // if not legendary item, decrease days remaining
             if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
                 items[i].sellIn = items[i].sellIn - 1;
             }
 
+            // if not after due date
             if (items[i].sellIn < 0) {
+                // if normal item
                 if (!items[i].name.equals("Aged Brie")) {
                     if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                         if (items[i].quality > 0) {
